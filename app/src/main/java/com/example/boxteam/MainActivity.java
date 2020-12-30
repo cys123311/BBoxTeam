d83286aab658f1bc245e96b604a36f4876b98dc0
sn <2297424289@qq.com>
2020-12-30 17:30:05 +0800

    sn

--- app/src/main/java/com/example/boxteam/MainActivity.java
+++ app/src/main/java/com/example/boxteam/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.boxteam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.QwertyKeyListener;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i( "TAG", "onCreate: "+ 123456 );
     }
 }

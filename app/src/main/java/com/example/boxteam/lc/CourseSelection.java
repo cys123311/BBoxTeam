88e19cf6be3eee124b351f1587610f243eb302d3
lc13633421480 <13633421480@163.com>
2021-01-03 10:54:00 +0800

    qq

---
+++ app/src/main/java/com/example/boxteam/lc/CourseSelection.java
@@ -0,0 +1,63 @@
+package com.example.boxteam.lc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+
+import com.example.boxteam.R;
+import com.example.boxteam.lc.base.BaseActivity;
+import com.example.boxteam.lc.interfaces.IBasePresenter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+/*
+ * 一对一课程
+ * */
+public class CourseSelection extends BaseActivity implements View.OnClickListener {
+    @BindView(R.id.content_input)
+    ConstraintLayout contentInput;
+    @BindView(R.id.bnt_ok)
+    Button bntOk;
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_courseselection;
+    }
+
+    @Override
+    protected IBasePresenter createPrenter() {
+        return null;
+    }
+
+
+
+    @Override
+    protected void initView() {
+        bntOk.setOnClickListener(this);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bnt_ok:
+//                startActivity(new Intent(CourseSelection.this,CourseFigure.class));
+                break;
+        }
+    }
+}
